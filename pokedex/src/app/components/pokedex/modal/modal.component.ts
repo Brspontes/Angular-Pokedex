1a8fb8b9410434bee6ced44a7a82bc0c7f95711c
Brspontes <brian.robert16@hotmail.com>
2021-02-19 19:47:45 -0300

    adicionando modal para detalhe pokedex

---
+++ pokedex/src/app/components/pokedex/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+  closeResult: string;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit(): void {
+  }
+}
